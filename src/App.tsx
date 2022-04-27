a85f59557fcaca2b4094d23a061be400719ebdbd
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-04-27 15:18:28 +0200

    logging handling form added, login error handling in function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getCredentials } from './utils/getCredentials';
 import { getAreasSecondary } from './utils/getAreasSecondary';
 import { getAreasData } from './utils/getAreasData';
@@ -8,61 +8,80 @@ const App = () => {
   const [key, setKey] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginState, setLoginState] = useState("not logged in");
 
   useEffect(() => {
     console.log(key);
   }, [key]);
 
-  const handleLogin = () => {
-    getCredentials(username, password, setKey);
+  const handleLogin = (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
+    getCredentials(username, password, setKey, setLoginState);
   };
 
-  const handleSecondary = () => {
+  const handleSecondary = (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
     getAreasSecondary(key);
   };
 
-  const handleData = () => {
+  const handleData = (e: React.SyntheticEvent<EventTarget>) => {
+    e.preventDefault();
     getAreasData(key);
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <div className="App-loginWrapper">
-          <div className="App-loginInnerWrapper">
-            <input
-              className="App-input"
-              name="username"
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-            <input
-              className="App-input"
-              name="password"
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-            <button
-              className="App-button"
-              onClick={handleLogin}>
-              LOG IN
-            </button>
-          </div>
-          <div className="App-loginInnerWrapper">
-            <button
-              className="App-button"
-              onClick={handleSecondary}
-            >
-              get /areas/secondary
-            </button>
-            <button
-              className="App-button"
-              onClick={handleData}
-            >
-              get /areas/data
-            </button>
-          </div>
-        </div>
+        <form>
+          <fieldset
+            className={`
+              App-loginWrapper 
+              ${loginState === "logged in" ? "App-success" : ""}
+              ${loginState === "login failed" ? "App-warning" : ""}
+          `}
+          >
+            <legend className={`App-legend`}>{loginState}</legend>
+            <div className={`App-loginInnerWrapper`}>
+              <input
+                className="App-input"
+                name="username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+              <input
+                className="App-input"
+                name="password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <button
+                className="App-button"
+                onClick={handleLogin}>
+                LOG IN
+              </button>
+            </div>
+            <div className="App-loginInnerWrapper">
+              <button
+                className="App-button"
+                disabled
+              >
+                get&nbsp;/areas/primary
+              </button>
+              <button
+                className="App-button"
+                onClick={handleSecondary}
+              >
+                get&nbsp;/areas/secondary
+              </button>
+              <button
+                className="App-button"
+                onClick={handleData}
+              >
+                get&nbsp;/areas/data
+              </button>
+            </div>
+          </fieldset>
+        </form>
       </header>
     </div>
   );
--- src/utils/getCredentials.ts
+++ src/utils/getCredentials.ts
@@ -1,17 +1,32 @@
 const url = `${process.env.REACT_APP_API_URL}/users/login/`;
 
-export const getCredentials = (username: string, password: string, setKey: (key: string) => void) => {
-  fetch(url, {
-    method: "POST",
-    headers: {
-      "Accept": "*/*",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      "username": username,
-      "password": password
-    }),
-  })
-    .then(response => response.json())
-    .then(data => setKey(data.key))
+export const getCredentials = async (
+  username: string,
+  password: string,
+  setKey: (key: string) => void,
+  setLoginState: (key: string) => void
+) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Accept": "*/*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "username": username,
+        "password": password
+      }),
+    })
+
+    if (!response.ok) {
+      throw new Error;
+    }
+
+    const data = await response.json();
+    setKey(data.key);
+    setLoginState("logged in");
+  } catch (error) {
+    setLoginState("login failed");
+  }
 };
