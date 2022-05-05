e579f7d8fdca4ee8f41ed806709aa611d9ab332a
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2022-05-05 09:10:26 +0100

    fix displaying plot on mobile

--- src/sections/chlorophyll/styled.tsx
+++ src/sections/chlorophyll/styled.tsx
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const ChlorophyllWrapper = styled.div`
   text-align: center;
   margin-bottom: 100px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}) {
+    margin: 0 -40px;
+  }
 `
